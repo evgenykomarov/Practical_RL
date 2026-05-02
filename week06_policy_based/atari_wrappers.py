6818e20dd7462156ec2011b04ba592d24d325996
evgenykomarov <54294403+evgenykomarov@users.noreply.github.com>
2026-05-02 12:45:15 +0200

    Update atari_wrappers.py

--- week06_policy_based/atari_wrappers.py
+++ week06_policy_based/atari_wrappers.py
@@ -8,7 +8,7 @@ from gymnasium import ObservationWrapper, RewardWrapper, Wrapper
 from gymnasium.spaces import Box
 from gymnasium.wrappers import RecordVideo
 # from shimmy.atari_env import AtariEnv
-from ale_py.gym.environment import AtariEnv
+from ale_py import AtariEnv
 from tensorboardX import SummaryWriter
 
 from env_batch import ParallelEnvBatch
